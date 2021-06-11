bb6c47101866b33994b383269e4537a8ea9d223d
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-06-11 17:57:32 +0530

    multi threading

---
+++ lib/src/main/java/com/EmployeePayroll_MultiThreading/EmployeePayrollDBService.java
@@ -0,0 +1,152 @@
+package com.EmployeePayroll_MultiThreading;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class EmployeePayrollDBService {
+	private static EmployeePayrollDBService employeePayrollDBService;
+	private static Logger log = Logger.getLogger(EmployeePayrollDBService.class.getName());
+	private int connectionCounter = 0;
+	Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+
+	// creating the object of Signature and getting instance
+	public static EmployeePayrollDBService getInstance() {
+		if (employeePayrollDBService == null) {
+			employeePayrollDBService = new EmployeePayrollDBService();
+		}
+		return employeePayrollDBService;
+	}
+
+	public synchronized Connection getConnection() throws SQLException {
+		connectionCounter++;
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "13041997@Mda";
+		// the DriverManager class will attempt to load available JDBC drivers
+		log.info("Processing Thread : " + Thread.currentThread().getName() + "Connecting to database : " + jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, password);
+		// which thread is actually going to execute my connection
+		log.info("Processing Thread : " + Thread.currentThread().getName() + " ID : " + connectionCounter
+				+ " Connection is successful! " + connection);
+		return connection;
+	}
+
+	public List<EmployeePayrollData> readData() {
+		String sql = "SELECT * FROM employee_payroll;";
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		try (Connection connection = this.getConnection();) {
+			// A SQL statement is precompiled and stored in a PreparedStatement object.
+			// This object can then be used to efficiently execute this statement multiple
+			// times.
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultSet = statement.executeQuery(sql);
+			employeePayrollList = this.getEmployeeData(resultSet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+	}
+
+	public EmployeePayrollData addEmployeeToPayrollDB(int emp_id, String name, String gender, double salary,
+			LocalDate startDate) {
+		int employeeId = -1;
+		EmployeePayrollData employeePayrollData = null;
+		String sql = String.format(
+				"INSERT INTO employee_payroll (name,gender,salary,start) VALUES ('%s','%s','%s','%s')", name, gender,
+				salary, Date.valueOf(startDate));
+		try (Connection connection = this.getConnection();) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			// generated keys should be made available for retrieval
+			int rowAffected = preparedStatement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+			if (rowAffected == 1) {
+				ResultSet resultSet = preparedStatement.getGeneratedKeys();
+				if (resultSet.next())
+					employeeId = resultSet.getInt(1);
+			}
+			employeePayrollData = new EmployeePayrollData(employeeId, name, gender, salary, startDate);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try (Connection connection = this.getConnection();) {
+			double deductions = salary * 0.2;
+			double taxablePay = salary - deductions;
+			double tax = taxablePay * 0.1;
+			double netPay = salary - tax;
+			String sql1 = String.format(
+					"INSERT INTO payroll_details(emp_id,basic_pay,deductions,taxable_pay,tax ,net_pay)VALUES (%s,%s,%s,%s,%s,%s)",
+					employeeId, salary, deductions, taxablePay, tax, netPay);
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			int rowAffected = preparedStatement.executeUpdate(sql1);
+			if (rowAffected == 1) {
+				employeePayrollData = new EmployeePayrollData(employeeId, name, gender, salary, startDate);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return employeePayrollData;
+	}
+
+	public synchronized int updateEmployeeData(String name, Double salary) {
+		return this.updateEmployeeDataUsingPreparedStatement(name, salary);
+	}
+
+	private int updateEmployeeDataUsingPreparedStatement(String name, Double salary) {
+		String sql = String.format("UPDATE employee_payroll SET salary=%.2f WHERE name='%s'", salary, name);
+		try (Connection connection = this.getConnection();) {
+			PreparedStatement prepareStatement = connection.prepareStatement(sql);
+			return prepareStatement.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public List<EmployeePayrollData> getEmployeeData(String name) {
+		List<EmployeePayrollData> employeePayrollList = null;
+		if (this.preparedStatement == null)
+			this.preparedStatementForEmployeeData();
+		try {
+			// Sets the designated parameter to the given Java String value
+			preparedStatement.setString(1, name);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			employeePayrollList = this.getEmployeeData(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+	}
+
+	private List<EmployeePayrollData> getEmployeeData(ResultSet resultSet) throws SQLException {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		while (resultSet.next()) {
+			int emp_id = resultSet.getInt("emp_id");
+			String name = resultSet.getString("name");
+			String gender = resultSet.getString("gender");
+			double salary = resultSet.getDouble("salary");
+			LocalDate startDate = resultSet.getDate("start").toLocalDate();
+			employeePayrollList.add(new EmployeePayrollData(emp_id, name, gender, salary, startDate));
+		}
+		return employeePayrollList;
+	}
+
+	private void preparedStatementForEmployeeData() {
+		try {
+			Connection connection = this.getConnection();
+			String sql = "SELECT * FROM employee_payroll WHERE name=?";
+			preparedStatement = connection.prepareStatement(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ lib/src/main/java/com/EmployeePayroll_MultiThreading/EmployeePayrollService.java
@@ -0,0 +1,133 @@
+package com.EmployeePayroll_MultiThreading;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class EmployeePayrollService {
+	private static Logger log = Logger.getLogger(EmployeePayrollService.class.getName());
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
+	}
+
+	private List<EmployeePayrollData> employeePayrollList;
+	private EmployeePayrollDBService employeePayrollDBService;
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this();
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	public EmployeePayrollService() {
+		employeePayrollDBService = EmployeePayrollDBService.getInstance();
+	}
+
+	public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) {
+		if (ioService.equals(IOService.DB_IO))
+			this.employeePayrollList = employeePayrollDBService.readData();
+		return this.employeePayrollList;
+	}
+
+	public void addEmployeeToPayroll(List<EmployeePayrollData> employeePayrollList) {
+		employeePayrollList.forEach(employeePayrollData -> {
+			log.info("Employee being added : " + employeePayrollData.name);
+			try {
+				this.addEmployeeToPayrollDB(employeePayrollData.emp_id, employeePayrollData.name,
+						employeePayrollData.gender, employeePayrollData.salary, employeePayrollData.startDate);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			log.info("Employee added : " + employeePayrollData.name);
+		});
+		log.info("" + this.employeePayrollList);
+	}
+
+	public void addEmployeeToPayrollWithThreads(List<EmployeePayrollData> asList) {
+		Map<Integer, Boolean> employeeAdditionStatus = new HashMap<>();
+		employeePayrollList.forEach(employeePayrollData -> {
+			// creating task using runnable to execute the thread
+			Runnable task = () -> {
+				// employee payroll object id is set to false because get is not added
+				employeeAdditionStatus.put(employeePayrollData.hashCode(), false);
+				log.info("Employee being added : " + Thread.currentThread().getName());
+				try {
+					this.addEmployeeToPayrollDB(employeePayrollData.emp_id, employeePayrollData.name,
+							employeePayrollData.gender, employeePayrollData.salary, employeePayrollData.startDate);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				employeeAdditionStatus.put(employeePayrollData.hashCode(), true);
+				log.info("Employee added : " + Thread.currentThread().getName());
+			};
+			// creating a thread and assigning to start the task
+			Thread thread = new Thread(task, employeePayrollData.name);
+			thread.start();
+		});
+		// keeping Main thread to wait
+		while (employeeAdditionStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		log.info("" + this.employeePayrollList);
+	}
+
+	private void addEmployeeToPayrollDB(int emp_id, String name, String gender, double salary, LocalDate startDate) {
+		employeePayrollList
+				.add(employeePayrollDBService.addEmployeeToPayrollDB(emp_id, name, gender, salary, startDate));
+	}
+
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.DB_IO))
+			return employeePayrollList.size();
+		return 0;
+	}
+
+	public void updateSalaryOfMultipleEmployees(Map<String, Double> employeeSalaryMap) {
+		Map<Integer, Boolean> salaryUpdateStatus = new HashMap<>();
+		employeeSalaryMap.forEach((employee, salary) -> {
+			Runnable salaryUpdate = () -> {
+				salaryUpdateStatus.put(employee.hashCode(), false);
+				log.info("Salary being updated : " + Thread.currentThread().getName());
+				this.updateEmployeeSalary(employee, salary);
+				salaryUpdateStatus.put(employee.hashCode(), true);
+				log.info("Salary updated : " + Thread.currentThread().getName());
+			};
+			Thread thread = new Thread(salaryUpdate, employee);
+			thread.start();
+		});
+		while (salaryUpdateStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		log.info("" + this.employeePayrollList);
+	}
+
+	public void updateEmployeeSalary(String name, double salary) {
+		int result = employeePayrollDBService.updateEmployeeData(name, salary);
+		if (result == 0)
+			return;
+		EmployeePayrollData employeePayrollData = this.getEmployeeData(name);
+		if (employeePayrollData != null)
+			employeePayrollData.salary = salary;
+	}
+
+	private EmployeePayrollData getEmployeeData(String name) {
+		return this.employeePayrollList.stream()
+				.filter(employeePayrollData -> employeePayrollData.name.equalsIgnoreCase(name)).findFirst()
+				.orElse(null);
+	}
+
+	public boolean checkEmployeePayrollInSyncWithDB(String name) {
+		List<EmployeePayrollData> employeeDataList = employeePayrollDBService.getEmployeeData(name);
+		return employeeDataList.get(0).equals(this.getEmployeeData(name));
+	}
+}
